c0070199c4f349b6c6780051b34a936e09324a68
jsills2 <jsills2592@gmail..com>
2025-04-11 11:45:35 -0400

    updated CSP to include google fonts

--- app.js
+++ app.js
@@ -28,9 +28,13 @@ app.use('/resources', express.static(path.join(__dirname, 'resources')));
 
 // Set Content Security Policy for fonts
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'self'; font-src 'self' https://exercise-tracker-app-5e089126146e.herokuapp.com;");
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; font-src 'self' https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com;"
+  );
   next();
 });
+
 
 
 // Define your routes
